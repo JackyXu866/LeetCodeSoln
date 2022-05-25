34749ba8589e0221d2896163e4f87234d3427299
Jacky Xu <33426411+JackyXu866@users.noreply.github.com>
2022-05-25 02:12:01 -0400

    Create 0300_LongestIncreasingSubsequence.cpp

---
+++ 0300_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> lis;
+        
+        for(int i : nums){
+            auto it = lower_bound(lis.begin(), lis.end(), i);
+            if(it == lis.end()) lis.push_back(i);
+            else *it = i;
+        }
+        
+        return lis.size();
+    }
+};
