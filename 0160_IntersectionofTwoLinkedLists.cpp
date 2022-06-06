96ca9f6b1cd172ca9ab522a5da830dc3dd5333fe
Jacky Xu <33426411+JackyXu866@users.noreply.github.com>
2022-06-06 19:09:05 -0400

    Update 0160_IntersectionofTwoLinkedLists.cpp

--- 0160_IntersectionofTwoLinkedLists.cpp
+++ 0160_IntersectionofTwoLinkedLists.cpp
@@ -11,10 +11,11 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* a = headA, *b = headB;
         while(a != b){
-            a = (a?a->next : headA);
-            b = (b?b->next : headB);
+            a = (a?a->next : headB);
+            b = (b?b->next : headA);
         }
         
         return a;
     }
 };
+
