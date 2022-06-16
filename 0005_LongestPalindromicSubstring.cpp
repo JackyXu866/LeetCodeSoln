d51bd05b8ab39948a512bfc2ef28638ce24af595
Jacky Xu <33426411+JackyXu866@users.noreply.github.com>
2022-06-15 20:33:00 -0400

    Create 0005_LongestPalindromicSubstring.cpp

---
+++ 0005_LongestPalindromicSubstring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestPalindrome(string& s) {
+        if(s == "") return "";
+        int r_b = 0, r_s = 1, b, e;
+        
+        for(int i=0; i<s.size(); i++){
+            expandPali(i, i, s, r_b, r_s);
+            expandPali(i, i+1, s, r_b, r_s);
+        }
+        
+        return s.substr(r_b, r_s);
+    }
+    
+private:
+    void expandPali(int b, int e, string& s, int& r_b, int& r_s){
+        while(b>=0 && e<s.size()){
+            if(s[b] == s[e]){
+                if(r_s < e-b+1){
+                    r_b = b;
+                    r_s = e-b+1;
+                }
+            } else break;
+            b--, e++;
+        }
+    }
+};
