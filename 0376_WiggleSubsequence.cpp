f88e8f551a29e57ff9b609e517813494bd02b73e
Jacky Xu <33426411+JackyXu866@users.noreply.github.com>
2022-07-03 03:13:32 -0400

    Create 0376_WiggleSubsequence.cpp

---
+++ 0376_WiggleSubsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int p = 1, n = 1;
+        
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]-nums[i-1] > 0){
+                p = n+1;
+            }else if(nums[i]-nums[i-1] < 0){
+                n = p+1;
+            }
+        }
+        
+        return max(p, n);
+    }
+};
